70ecc9b13eb5f67b67ee06feaaef69b8e06bea09
Jeseekia Vaughn <jeseekia@gmail.com>
2015-08-24 02:21:16 -0400

    Completed tasks using jQuery.

--- lab5/scripts.js
+++ lab5/scripts.js
@@ -1,13 +1,26 @@
 // Each < li > should fade to 0.25 opacity when the user hovers over it.
+$("li").hover(
+  function() {
+    $(this).fadeTo("slow",0.25);
+  }, function() {
+    $(this).fadeTo("slow",1);
+  }
 
-
-
-// Each < li > should change background colors when clicked on.
-
-
-
-// When the user clicks on a < li >, the word 'Clicked!' should appear inside it.
-  
-
+);
 
 // Changes triggered by a click should change back when the < li > is clicked again.
+
+  // Each < li > should change background colors when clicked on.
+  $("li").click(
+    function() {$(this).toggleClass("regular clicked");
+  });
+
+
+  // When the user clicks on a < li >, the word 'Clicked!' should appear inside it.
+  $("li").on("click", function(){
+    if ($(this).children().length>0) {
+      $(this).find( $("span:last").remove() );
+    } else {
+      $(this).append( $("<span> Clicked!</span>") );
+    }
+  });
