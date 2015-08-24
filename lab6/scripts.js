41a7a4477e20bdc2bddeb3672d0c54dfc3fdba53
Jeseekia Vaughn <jeseekia@gmail.com>
2015-08-24 11:06:07 -0400

    Adding for loop to iterate through reddit page posts array. Adding console.log() to the for loop to test that array objects are being accessed. Chaining .done().fail().always() with console.log() messages. Removing the var author declaration in .getJSON(). Adding var author, score, and title declarations to global scope.

--- lab6/scripts.js
+++ lab6/scripts.js
@@ -1,4 +1,7 @@
 var xhr = new XMLHttpRequest();
+var author;
+var score;
+var title;
 
 xhr.onload = function() {
   if (xhr.status === 200) {
@@ -7,11 +10,22 @@ xhr.onload = function() {
 
     $.getJSON("http://www.reddit.com/r/aww.json", function(reddit){
       console.log(reddit.data.children[0].data.author);
+
+      for (var i = 0; i < reddit.data.children.length; i++) {
+        console.log(reddit.data.children[i]);
+      }
+
+      author = reddit.data.children[0].data.author;
+    })
+    .done(function() {
+      console.log( "second success" );
+    })
+    .fail(function() {
+      console.log( "error" );
+    })
+    .always(function() {
+      console.log( "complete" );
     });
-
-
-    var author = data.children[0].data.author;
-
 
   }
 }
