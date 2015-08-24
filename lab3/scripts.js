f1ef97bcb8fb6d5442df1264dd7e9a5d0766724d
Jeseekia Vaughn <jeseekia@gmail.com>
2015-08-23 21:05:28 -0400

    Changing .available to now refer to .btn-success

--- lab3/scripts.js
+++ lab3/scripts.js
@@ -1,2 +1,2 @@
-// Add listening function for ".available".onclick to show an html form
+// Add listening function for ".btn-succes".onclick to show an html form
 // Try Use visibility: hidden or display: none and use function to change value on form
