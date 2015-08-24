80777891e092fd6b4e16253a0bebb76fc90a2ace
Jeseekia Vaughn <jeseekia@gmail.com>
2015-08-23 23:04:43 -0400

    Adding a variable to store the clicked button name. Added empty array to store seat reservations. Added a constructor for reservedSeat objects where objects are automatically added to the array. Adding a function that creates a reserved seat object when the form is submitted.

--- lab4/scripts.js
+++ lab4/scripts.js
@@ -5,10 +5,44 @@ var reserveButton = document.getElementsByClassName('btn-success');
 // var reserveButton = document.getElementById('btn');
 // var reserveButton = document.getElementsByTagName('button');
 
-
+var clickedButtonName;
 for (var i=0; i < reserveButton.length; i++) {
   reserveButton[i].onclick = function(event){
     // event.preventDefault();
     reservationForm.style.visibility='visible';
+    // var clickedButtonName = reserveButton[i].name;
+    clickedButtonName = this.name;
+    return clickedButtonName;
   }
+}
+
+// Reserved Seat List Array
+var reservedSeatList = [];
+
+//Reserved Seat Constructor
+function reservedSeat(firstName,lastName,seatNumber,email) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.seatNumber = seatNumber;
+  this.email = email;
+  reservedSeatList.push(this);
+}
+
+// Outcome is the new reserved seat object created
+var outcome;
+// formItems is the submit button element
+var formItems = document.getElementById('addbutton');
+// Listening for a click on the submit button
+formItems.onclick= function(event){
+  event.preventDefault();
+  var fname = document.getElementById('firstName').value;
+  console.log(fname);
+  var lname = document.getElementById('lastName').value;
+  console.log(lname);
+  var emaill = document.getElementById('email').value;
+  console.log(emaill);
+  var seat = clickedButtonName;
+  console.log(seat);
+  outcome = new reservedSeat(fname,lname,seat,emaill);
+  // console.log(outcome);
 }
