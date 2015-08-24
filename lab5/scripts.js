89207a76c8af89a921c9d058cefa6b5040ff07e8
Jeseekia Vaughn <jeseekia@gmail.com>
2015-08-24 00:42:07 -0400

    Adding comments for functionality that should be included.

---
+++ lab5/scripts.js
@@ -0,0 +1,13 @@
+// Each < li > should fade to 0.25 opacity when the user hovers over it.
+
+
+
+// Each < li > should change background colors when clicked on.
+
+
+
+// When the user clicks on a < li >, the word 'Clicked!' should appear inside it.
+  
+
+
+// Changes triggered by a click should change back when the < li > is clicked again.
